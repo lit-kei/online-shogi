08fd56bf391a7c9ce3caece2712de4a4a65b3439
Kei <puyoteto123@yahoo.co.jp>
2025-11-05 15:18:06 +0900

    update

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+const initialSetup = [
+[ {t:2,p:'white'},{t:3,p:'white'},{t:4,p:'white'},{t:5,p:'white'},{t:15,p:'white'},{t:5,p:'white'},{t:4,p:'white'},{t:3,p:'white'},{t:2,p:'white'} ],
+[ null,{t:7,p:'white'},null,null,null,null,null,{t:6,p:'white'},null ],
+[ {t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'},{t:1,p:'white'} ],
+[ null,null,null,null,null,null,null,null,null ],
+[ null,null,null,null,null,null,null,null,null ],
+[ null,null,null,null,null,null,null,null,null ],
+[ {t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'},{t:1,p:'black'} ],
+[ null,{t:6,p:'black'},null,null,null,null,null,{t:7,p:'black'},null ],
+[ {t:2,p:'black'},{t:3,p:'black'},{t:4,p:'black'},{t:5,p:'black'},{t:14,p:'black'},{t:5,p:'black'},{t:4,p:'black'},{t:3,p:'black'},{t:2,p:'black'} ],
+];
+
 let playerName = localStorage.getItem('shogi-name');
 if (!playerName) {
     playerName = prompt("名前を入力してください：");
@@ -15,7 +27,7 @@ if (!playerName) {
 document.getElementById('createRoom').addEventListener('click', async () => {
     const { data, error } = await supabase
         .from("rooms")
-        .insert([{ nowPlayer: true }])
+        .insert([{ boardState: initialSetup, nowPlayer: true }])
         .select("id")
         .single();
 
@@ -31,3 +43,4 @@ document.getElementById('createRoom').addEventListener('click', async () => {
     window.location.href = `./game.html?room=${roomId}`;
     
 });
+
